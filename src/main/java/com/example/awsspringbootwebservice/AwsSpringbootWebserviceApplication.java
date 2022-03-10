2998d469b1c9ecda59519bb249f13b8cf8a130dd
jung <wjddn3711@naver.com>
2022-03-10 21:56:24 +0900

    Starting springboot project

---
+++ src/main/java/com/example/awsspringbootwebservice/AwsSpringbootWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.awsspringbootwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsSpringbootWebserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsSpringbootWebserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/awsspringbootwebservice/AwsSpringbootWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.awsspringbootwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsSpringbootWebserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
